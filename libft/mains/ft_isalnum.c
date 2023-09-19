27069478ab6f89f0254309eee1dc2a181c195522
Kelho <Kelho88@pm.me>
2023-09-19 17:26:22 +0100

    added conditionals

--- libft/mains/ft_isalnum.c
+++ libft/mains/ft_isalnum.c
@@ -4,5 +4,20 @@ int ft_isalnum(int c)
 	{
 		return (1);
 	}
+
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return(0);
+	}
+	
+	
 	
 }
